4c3e022bfc751b514201d775d601c71b3d8edeff
rdunlocked18 <rohitdaftari3@gmail.com>
2020-12-07 22:03:48 +0530

    Initial Commit

---
+++ EladTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EladTraining
+//
+//  Created by Rohit Daftari on 07/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
